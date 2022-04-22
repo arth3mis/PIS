b2a02cf7a900496cfbb419880104ca87feacbe68
Arthur <af@isi-sys.com>
2022-04-22 11:22:00 +0200

    Boilerplate 2

--- src/aj_vw1/Boilerplate.java
+++ src/aj_vw1/Boilerplate.java
@@ -1,26 +1,71 @@
 package aj_vw1;
 
 import processing.core.PApplet;
+import processing.event.MouseEvent;
 
 public class Boilerplate extends PApplet {
 
     public static void main(String[] args) {
-        PApplet.runSketch(new String[]{""}, new ArtSketch());
+        PApplet.runSketch(new String[]{""}, new Boilerplate());
     }
 
+    @Override
     public void settings() {
-        size(1000, 900);
+        //fullScreen();
+        size(800, 450);
     }
 
+    // variables
+    //
+
+
+    @Override
     public void setup() {
-
+        frameRate(60);
+        smooth(8);
     }
 
+    @Override
     public void draw() {
+        println(mouseX, mouseY);
+    }
+
+    @Override
+    public void mousePressed() {
+        if (mouseButton == LEFT) {
+            println("LMB");
+        } else if (mouseButton == RIGHT) {
+            println("RMB");
+        } else {
+            println("?MB");
+        }
+    }
+
+    @Override
+    public void mouseReleased() {
 
     }
 
+    float mouseWheelDelta = 0;
 
+    @Override
+    public void mouseWheel(MouseEvent event) {
+        mouseWheelDelta += event.getCount();
+    }
 
+    boolean[] keyPressed = new boolean[1000];
 
+    @Override
+    public void keyPressed() {
+        if (key == CODED) {
+            keyPressed[keyCode] = true;
+        }
+    }
+
+    @Override
+    public void keyReleased() {
+        if (key == CODED) {
+            keyPressed[keyCode] = false;
+        }
+    }
 }
